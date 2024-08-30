865441719afa3dd88f1138581c4ed3fb88e70aba
TommyBruce <3268437983@qq.com>
2024-08-30 18:10:36 +0800

    Update test.c

--- test_8_30/test_8_30/test.c
+++ test_8_30/test_8_30/test.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 int main()
 {
-	printf("hello,world\n");
+	int arr[] = { 1,2,3,4,5,6,7,8,9 };
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i=0;i<sz;i++)
+	{
+		printf("%d ", arr[i]);
+	}
 	return 0;
 }
